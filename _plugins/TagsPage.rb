20d93ed74b00b38c31e4cd4f81ed8d4d151e87a2
hwshqtb <1353736321@qq.com>
2025-12-18 20:45:37 +0800

    update

--- _plugins/TagsPage.rb
+++ _plugins/TagsPage.rb
@@ -1,31 +1,32 @@
 module Jekyll
-    class TagsPage < Page
-      def initialize(site, base, dir, tag)
-        @site = site
-        @base = base
-        @dir = dir
-        @name = tag + '.html'
-        self.process(@name)
-        self.read_yaml(File.join(base, '_layouts'), 'tags-page.html')
-        self.data['tag'] = tag
-        self.data['title'] = "拥有#{tag} tag的文章/页面"
-      end
+  class TagsPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = tag + '.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tags-page.html')
+      self.data['tag'] = tag
+      self.data['title'] = "拥有#{tag} tag的文章"
     end
-    class TagGenerator < Generator
-      safe true
-      def generate(site)
-        if site.layouts.key? 'tags-page'
-          site.tags.keys.each do |tag|
-            write_tag_index(site, 'tags', tag)
-          end
+  end
+
+  class TagGenerator < Generator
+    safe true
+    def generate(site)
+      if site.layouts.key? 'tags-page'
+        site.tags.keys.each do |tag|
+          write_tag_index(site, 'tags', tag)
         end
       end
-      def write_tag_index(site, dir, tag)
-        index = TagsPage.new(site, site.source, dir, tag)
-        index.render(site.layouts, site.site_payload)
-        index.write(site.dest)
-        site.pages << index
-      end
     end
-  
+    def write_tag_index(site, dir, tag)
+      index = TagsPage.new(site, site.source, dir, tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
   end
+
+end
